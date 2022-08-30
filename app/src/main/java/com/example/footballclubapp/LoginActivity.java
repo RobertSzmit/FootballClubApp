a9d863a9d2904de98a0e3983895ef718f71659b6
Arkadiusz Antonkiewicz <arkadiusz.antonkiewicz@op.pl>
2022-08-30 20:42:09 +0200

    Add login and register activities (everything working well)

--- app/src/main/java/com/example/footballclubapp/LoginActivity.java
+++ app/src/main/java/com/example/footballclubapp/LoginActivity.java
@@ -1,19 +1,32 @@
 package com.example.footballclubapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText loginEmail, loginPassword;
     Button loginBtn;
     TextView forgotPassword, needAnAccount;
+    FirebaseAuth auth;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +39,9 @@ public class LoginActivity extends AppCompatActivity {
         forgotPassword = findViewById(R.id.forgot_password);
         needAnAccount = findViewById(R.id.need_an_account);
 
+        progressDialog = new ProgressDialog(this);
+        auth = FirebaseAuth.getInstance();
+
         needAnAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,5 +49,62 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = loginEmail.getText().toString().trim();
+                String password = loginPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    loginEmail.setError(getString(R.string.comm7));
+                } else if (TextUtils.isEmpty(password)) {
+                    loginPassword.setError(getString(R.string.comm8));
+                } else {
+                    Login (email, password);
+                }
+            }
+        });
+
     }
+
+    private void Login(String email, String password) {
+
+        progressDialog.setTitle(R.string.comm4);
+        progressDialog.show();
+
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            progressDialog.dismiss();
+                            Toast.makeText(LoginActivity.this, (R.string.comm5), Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        } else {
+                            Toast.makeText(LoginActivity.this, (R.string.comm6), Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
+                    }
+                });
+    }
+
+    // in this place we will check, if the user is already logged in - to take him to MainActivity
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = auth.getCurrentUser();
+
+        if (user != null) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        }
+    }
+
 }
--- app/src/main/java/com/example/footballclubapp/MainActivity.java
+++ app/src/main/java/com/example/footballclubapp/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.footballclubapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.action_logout) {
+            auth.signOut();
+            startActivity (new Intent(MainActivity.this, LoginActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
